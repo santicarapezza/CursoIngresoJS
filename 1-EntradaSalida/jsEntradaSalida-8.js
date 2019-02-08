57982212065f4d6f7f2570c8e9ebb0f3eafe575e
alumno <alumno>
2019-02-08 11:57:58 -0300

    termine el 8

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -5,10 +5,25 @@ ej.: "El resto es 0 ."*/
 function SacarResto()
 {
 	
+	var dividendo;
+	var divisor;
+	var divi;
+
+	dividendo=numeroDividendo.value;
+	divisor=numeroDivisor.value;
+	
+
+
+	dividendo=parseInt(dividendo);
+	divisor=parseInt(divisor);
+	
+
+	divi=(dividendo%divisor);
+
+	alert("el resto es: "+divi);
 
 
 
-	//test
 
 
 
