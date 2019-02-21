f5b4b42b234794dfca2d0f65d879caaf135e7c83
alumno <santii.ivan@gmail.com>
2019-02-21 10:18:31 -0300

    punto 4

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,6 +4,44 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
+
+	switch(mesDelAño) {
+
+	case"Febrero":
+
+	alert("si tiene 28 días.")
+
+	break
+
+	case"Enero":
+	case"Marzo":
+	case"Mayo":
+	case"Julio":
+	case"Agosto":
+	case"Octubre":
+	case "Diciembre":
+
+	alert("si tiene 31 días.")
+
+	break
+
+	default:
+
+	alert("si tiene 30 días.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
 	
 	
 
