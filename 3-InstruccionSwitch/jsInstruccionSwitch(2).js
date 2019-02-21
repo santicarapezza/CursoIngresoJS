2b4f3141b4b66a28c02294d763d6ac65773c01e7
alumno <santii.ivan@gmail.com>
2019-02-21 10:03:35 -0300

    punto 2

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,8 +3,39 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
 
+
+ 	switch(mesDelAño){
+
+ 	case "Julio":
+	case "Agosto":
+ 	
+ 	alert("Abrigate que hace frio")
+
+ 	break
+
+ 	case "Enero":
+ 	case "Febrero":
+ 	case "Marzo":
+ 	case "Abril":
+ 	case "Mayo":
+ 	case "Junio":
+
+ 	alert("Falta para el invierno.")
+
+ 	break
+
+ 	default:
+ 
+ 	alert("Ya pasamos el frio, ahora calor!!!.")
+
+
+
+
+
+
+
+ 	}
 
 
 
