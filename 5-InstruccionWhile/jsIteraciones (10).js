1ac5586cc1ca1920eb17b8567252f6f16b417c39
alumno <alumno>
2019-02-28 12:10:20 -0300

    while10

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,82 @@
 function mostrar()
 {
-
-	var contador=0;
-	//declarar contadores y variables 
-	
 	var respuesta="si";
+	var contador;
+	var respuesta;
+	var sumaPositivo;
+	var sumaNegativo;
+	var contPositivo;
+	var contNegativo;
+	var contCeros;
+	var cantPares;
+	var promedioNegativo;
+	var promedioPositivo;
+	
+	contador=0;
+	sumaNegativo=0;
+	sumaPositivo=0;
+	contNegativo=0;
+	contPositivo=0;
+	contCeros=0;
+	cantPares=0;
+	promedioNegativo=0;
+	promedioPositivo=0;
 
 	while(respuesta!="no")
 	{
-		
-	
+		numero=prompt("ingrese numero");
+		numero=parseInt(numero);
+
+		if(numero<0)
+		{
+			sumaNegativo=sumaNegativo+numero;
+			contNegativo++;
+		}
+		else
+		{
+			if(numero>0)
+			{
+				sumaPositivo=sumaPositivo+numero;
+				contPositivo++;
+			}
+			else
+			{
+				contCeros++;
+			}	
+		}
+
+		if(numero%2==0)
+		{
+			cantPares++;
+		}
+
+
+
+
+
+		respuesta=prompt("ingrese no para salir",);
 	}
 
+	promedioNegativo=sumaNegativo/contNegativo;
+	promedioPositivo=sumaPositivo/contPositivo;
 
 
 
+
+
+
+
+
+
+
+
+
+	console.log("suma de negativos: "+sumaNegativo);
+	console.log("suma de positivos: "+sumaPositivo);
+	console.log("contador de negativos "+contNegativo);
+	console.log("contador de positivos "+contPositivo);
+	console.log("contador de ceros "+contCeros);
+	console.log("cantidad de pares "+cantPares);
+	console.log("promedioNegativo "+promedioNegativo);
+	console.log("promedioPositivo "+promedioPositivo);
 }//FIN DE LA FUNCIÓN
